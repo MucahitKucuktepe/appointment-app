31a07f83c283b2583b8033cec73393c95ad87f62
MucahitKucuktepe <mehmet20132204@gmail.com>
2023-12-13 18:54:50 +0300

    modal handle start

--- src/components/AddModal.jsx
+++ src/components/AddModal.jsx
@@ -1,24 +1,31 @@
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import Form from "react-bootstrap/Form"
+import { useState } from "react";
 
-function AddModal({ show, handleClose }) {
+function AddModal({ show, handleClose, drName }) {
+    const [name, setName]=useState("")
+    const [date, setDate]= useState("")
+    const handleSubmit=(e)=>{
+        e.preventDefault()
+        console.log(name,date);
+    }
   return (
     <>
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Appointment for dr name</Modal.Title>
+          <Modal.Title>Appointment for {drName} </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <Form.Group className="mb-3" controlId="name">
               <Form.Label>Patient Name</Form.Label>
-              <Form.Control type="text" placeholder="Enter your name" required/>
+              <Form.Control onChange={(e)=>setName(e.target.value)} type="text" placeholder="Enter your name" required/>
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="datetime-local" placeholder="Date" required />
+              <Form.Control onChange={(e)=>setDate(e.target.value)} type="datetime-local" placeholder="Date" required />
             </Form.Group>
             <div className="text-center">
               <Button variant="success" type="submit" className="me-2">
--- src/components/AppointmentList.jsx
+++ src/components/AppointmentList.jsx
@@ -1,13 +1,36 @@
-import Container from "react-bootstrap/Container"
+import { Col, Row } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import { TiDeleteOutline } from "react-icons/ti";
 
-const AppointmentList = () => {
+const AppointmentList = ({ appointments }) => {
   return (
     <Container className="p-2">
       <h3 className="display-6 mb-2" style={{ color: "rgb(166, 18, 189)" }}>
         Appointment List
       </h3>
+      {appointments.map(({ id, patient, consulted, doctor, day }) => (
+        <div
+          key={id}
+          className={consulted ? "appointments consulted" : "appointments"}
+          role="button"
+        >
+          <Row className="justify-content-between align-items-center">
+            <Col>
+              <h4>{patient} </h4>
+              <h5>{doctor} </h5>
+            </Col>
+            <Col>
+              <h5>Date: {day.toLocaleDateString("tr")}</h5>
+              <h6>Time: {day.toLocaleTimeString("tr")}</h6>
+            </Col>
+            <Col className="text-end">
+              <TiDeleteOutline className="text-danger fs-2 " type="button" />
+            </Col>
+          </Row>
+        </div>
+      ))}
     </Container>
-  )
-}
+  );
+};
 
-export default AppointmentList
+export default AppointmentList;
